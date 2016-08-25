bd0a3febf9da53bc411a0d790209143566192a71
Alvaro Flores and Francis Prus <alvaro@>
2016-08-25 13:36:14 -0700

    create working javascript functionality for single die pig dice.

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,72 @@
 // business logic
-
-var currentscore = 0;
+var score = 0;
+var totalscore = 0;
+var totalscore2 = 0;
 var turn = 0;
-
 
 var Roll = function () {
   this.total =  0;
   this.dice = 0;
+  this.roll = 0;
 }
 
 Roll.prototype.addroll = function () {
   this.roll =  Math.floor(Math.random() * 6) + 1;
-  if (this.roll === 1) {
-    this.total -= this.roll;
-    alert('you lose');
-  }
-  this.total += this.roll;
+    if (this.roll === 1) {
+      this.total = 0;
+      alert('Next Player');
+  } if (this.roll !== 1){
+    this.total += this.roll;
+}
 };
-
-var player1 = 0;
-var player2 = 0;
 
 var rollDice =  new Roll();
 var rollDice2 = new Roll();
 
+
 // front end;
+// player1
 $(document).ready(function() {
   $('form#player1').submit(function(event){
     event.preventDefault();
-    rollDice.addroll()
-    $('#dice1').text(rollDice.roll);
-    $('#score1').text(rollDice.total);
+    if(turn === 0)
+    {
+      rollDice.addroll()
+    }
+    $('#dice1').append(rollDice.roll + " ");
+    $('#cScore1').text(rollDice.total);
   });
 
+// player2
   $('form#player2').submit(function(event) {
     event.preventDefault();
+    if (turn === 0){
     rollDice2.addroll();
-    $('#dice2').text(rollDice2.roll);
-    $('#score2').text(rollDice2.total);
-
+  }
+    $('#dice2').append(rollDice2.roll + " ");
+    $('#cScore2').text(rollDice2.total);
   });
+  // hold button
+    $('form#hold1').submit(function(event){
+      event.preventDefault();
+      totalscore += rollDice.total;
+      rollDice.total = 0;
+      $('#totalScore1').text(totalscore);
+      $('#totalScore1').text();
+      alert("Player 2's turn");
+      if (totalscore >= 100){
+        alert("Player one wins!!")
+      }
+    });
+    $('form#hold2').submit(function(event){
+      event.preventDefault();
+      totalscore2 += rollDice2.total;
+      rollDice2.total = 0;
+      $('#totalScore2').text(totalscore2);
+      $('#totalScore2').text();
+      alert("Player 1's turn");
+      if (totalscore2 >= 100){
+        alert("Player two wins!!")
+      }
+    });
 });
